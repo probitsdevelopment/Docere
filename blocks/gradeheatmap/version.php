bdedcec54b11a914d84380b7a56d6e68d510567e
Bindu Jagannath <probitsdevelopment@gmail.com>
2025-08-20 13:35:47 +0530

    the changes of heatmap second

--- blocks/gradeheatmap/version.php
+++ blocks/gradeheatmap/version.php
@@ -1,9 +1,8 @@
-
 <?php
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_gradeheatmap';
-$plugin->version   = 2025082001;   // increment on each change
+$plugin->version   = 2025091500;   // Incremented to a higher version
 $plugin->requires  = 2022041900;   // Moodle 4.0+
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.1';        // Updated release number
