c6ae9f1357bedee701990503fd2cd35b90966483
Bindu Jagannath <probitsdevelopment@gmail.com>
2025-08-20 11:14:20 +0530

    some files name changes

--- blocks/grade_heatmap/version.php
+++ blocks/grade_heatmap/version.php
@@ -1,5 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
+
 $plugin->component = 'block_grade_heatmap';
-$plugin->version   = 2025082001;  // YYYYMMDDXX
-$plugin->requires  = 2022041900;  // Moodle 4.0+
+$plugin->version   = 2025082002;   // YYYYMMDDXX
+$plugin->requires  = 2022041900;   // Moodle 4.0+
