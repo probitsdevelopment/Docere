6248726f374468935c98c34849fa2e537363f1f9
Bindu Jagannath <probitsdevelopment@gmail.com>
2025-08-20 11:49:46 +0530

    the changes happens

--- blocks/gradeheatmap/version.php
+++ blocks/gradeheatmap/version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component = 'block_gradeheatmap'; // Must match folder name pattern block_[name].
-$plugin->version   = 2025081209;           // Increment this on each update (YYYYMMDDHH).
+$plugin->version   = 2025088069;           // Increment this on each update (YYYYMMDDHH).
 $plugin->requires  = 2022041900;           // Moodle 4.0+ (adjust if your site is older/newer).
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1'; 
