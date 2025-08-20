f72cec0616a85d5692cdfad8f3a076d577e24930
Bindu Jagannath <probitsdevelopment@gmail.com>
2025-08-20 13:15:17 +0530

    the changes deleted heatmap

--- blocks/gradeheatmap/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'block_gradeheatmap';
-$plugin->version   = 2025089991;   // increment on each change
-$plugin->requires  = 2022041900;   // Moodle 4.0+
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0';
