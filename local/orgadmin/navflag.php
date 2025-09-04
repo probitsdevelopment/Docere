cf25d880674e279765976b4903df5dd00129b837
Bindu Jagannath <probitsdevelopment@gmail.com>
2025-09-04 12:49:28 +0530

    the fixing of add user second time

--- local/orgadmin/navflag.php
+++ local/orgadmin/navflag.php
@@ -1,7 +1,20 @@
-
 <?php
+// File: local/orgadmin/navflag.php
 require_once(__DIR__ . '/../../config.php');
+require_login();
+
+$has = has_capability('local/orgadmin:adduser', context_system::instance());
+if (!$has) {
+    foreach (core_course_category::get_all() as $cat) {
+        if (has_capability('local/orgadmin:adduser', context_coursecat::instance($cat->id))) {
+            $has = true;
+            break;
+        }
+    }
+}
+
 header('Content-Type: application/javascript');
-if (isloggedin() && has_capability('local/orgadmin:adduser', context_system::instance())) {
-    echo 'document.body.classList.add("orgadmin");';
+if ($has) {
+    // Add the class early; works even before <body> is parsed.
+    echo 'document.documentElement.classList.add("orgadmin");';
 }
